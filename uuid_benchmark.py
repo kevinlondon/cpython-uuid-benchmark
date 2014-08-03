d1cce9700b7ac91b06bfc39716e22e8da554b857
kevinlondon <kevinlondon@gmail.com>
2014-08-03 14:07:21 -0700

    Rearranged the code to be more readable.

--- uuid_benchmark.py
+++ uuid_benchmark.py
@@ -6,13 +6,12 @@ from binascii import unhexlify
 
 N = int(sys.argv[1])
 
+
 class UpdatedUUID(uuid.UUID):
     @property
     def bytes(self):
         return unhexlify(self.hex)  
 
-original_uuid = uuid.uuid4()
-updated_uuid = UpdatedUUID(hex=original_uuid.hex)
 
 def benchmark(uuid_object):
     t = time.time()
@@ -22,5 +21,7 @@ def benchmark(uuid_object):
     print('%.3f microseconds' % ((time.time() - t) * 1000000.0 / N))
 
 
+original_uuid = uuid.uuid4()
+updated_uuid = UpdatedUUID(hex=original_uuid.hex)
 benchmark(original_uuid)
 benchmark(updated_uuid)
