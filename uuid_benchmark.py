246fd36786a01ee8627501a62ae9963f259197a8
kevinlondon <kevinlondon@gmail.com>
2014-08-03 14:02:57 -0700

    Initial test for comparing UUID.bytes to a custom UUID with unhexlify.

---
+++ uuid_benchmark.py
@@ -0,0 +1,26 @@
+import uuid
+import time
+import sys
+from binascii import unhexlify
+
+
+N = int(sys.argv[1])
+
+class UpdatedUUID(uuid.UUID):
+	@property
+	def bytes(self):
+		return unhexlify(self.hex)	
+
+original_uuid = uuid.uuid4()
+updated_uuid = UpdatedUUID(hex=original_uuid.hex)
+
+def benchmark(uuid_object):
+	t = time.time()
+	for x in xrange(N):
+		uuid_object.bytes
+
+	print('%.3f microseconds' % ((time.time() - t) * 1000000.0 / N))
+
+
+benchmark(original_uuid)
+benchmark(updated_uuid)
